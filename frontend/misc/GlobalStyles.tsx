ac6045a7e1d89b7615f061ef93887ea9a858d65f
Piotr Bartoszewski <bartoszewskip1000@gmail.com>
2020-06-23 14:05:15 +0200

    Import all needed fonts

--- frontend/misc/GlobalStyles.tsx
+++ frontend/misc/GlobalStyles.tsx
@@ -1,8 +1,13 @@
-import {Global, css} from '@emotion/core'
+import {css, Global} from '@emotion/core'
 import {FC} from 'react'
 
 export const GlobalStyles: FC = () => <Global styles={css`
-@import "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap";
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap');
 
 *, *::before, *::after {
   margin: 0;
