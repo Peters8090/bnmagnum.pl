9e83fa2edbdc1c39719a0c8ab6622d0fb9003189
Piotr Bartoszewski <bartoszewskip1000@gmail.com>
2020-11-30 15:54:08 +0100

    Add fetching offers

--- frontend/pages/wyszukiwarka-ofert/[[...offerName]].tsx
+++ frontend/pages/wyszukiwarka-ofert/[[...offerName]].tsx
@@ -1,53 +1,84 @@
-import css from '@emotion/css'
-import {RouteType} from '../../interfaces and types/RouteType'
-import {OfferList} from '../../components/pages/wyszukiwarka-ofert/OfferList/OfferList'
-import {OfferDetails} from '../../components/pages/wyszukiwarka-ofert/OfferDetails/OfferDetails'
-import {useCurrentNavigationHeight} from '../../hooks/useCurrentNavigationHeight'
-import {useTheme} from '@material-ui/core/styles'
-import {useRouter} from 'next/router'
-import {useMediaQuery} from '@material-ui/core'
-import {HiddenCond} from '../../components/shared/HiddenCond/HiddenCond'
+import css from "@emotion/css";
+import { RouteType } from "../../interfaces and types/RouteType";
+import { OfferList } from "../../components/pages/wyszukiwarka-ofert/OfferList/OfferList";
+import { OfferDetails } from "../../components/pages/wyszukiwarka-ofert/OfferDetails/OfferDetails";
+import { useCurrentNavigationHeight } from "../../hooks/useCurrentNavigationHeight";
+import { useTheme } from "@material-ui/core/styles";
+import { useRouter } from "next/router";
+import { useMediaQuery } from "@material-ui/core";
+import { HiddenCond } from "../../components/shared/HiddenCond/HiddenCond";
+import { GetServerSideProps } from "next";
+import axios from "axios";
+import queryString from "query-string";
 
 export const useOfferName = () => {
-    const {query} = useRouter()
-    return query.offerName!?.[0]
-}
+  const { query } = useRouter();
+  return query.offerName!?.[0];
+};
 
 const OfferSearch: RouteType = () => {
-    const theme = useTheme()
-    const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
-    const navHeight = useCurrentNavigationHeight()
-    const offerName = useOfferName()
-    const styles = {
-        root: css`    	    
-            background-color: #E0E0E0;
-            
-            ${theme.breakpoints.up('md')} {
-    	        display: grid;
-    	        height: calc(100vh - ${navHeight}px);
-    	        
-    	        grid-template-columns: 1fr 1fr;
-    	        & > * {
-    	          outline: 0.5px solid gray;
-    	          overflow-y: scroll;
-    	        }
-            }
-    	`,
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const navHeight = useCurrentNavigationHeight();
+  const offerName = useOfferName();
+  const styles = {
+    root: css`
+      background-color: #e0e0e0;
+
+      ${theme.breakpoints.up("md")} {
+        display: grid;
+        height: calc(100vh - ${navHeight}px);
+
+        grid-template-columns: 1fr 1fr;
+        & > * {
+          outline: 0.5px solid gray;
+          overflow-y: scroll;
+        }
+      }
+    `,
+  };
+
+  return (
+    <div css={styles.root}>
+      <HiddenCond
+        condition={isMobile ? !!offerName : false}
+        implementation="css"
+      >
+        <OfferList />
+      </HiddenCond>
+      <HiddenCond
+        condition={isMobile ? !offerName : !offerName}
+        implementation="css"
+      >
+        <OfferDetails />
+      </HiddenCond>
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const query = (() => {
+    if (context.req.url) {
+      const q = queryString.extract(context.req.url);
+      if (q) {
+        return "?" + q;
+      }
+      return "";
     }
+    return "";
+  })();
 
-    return (
-        <div css={styles.root}>
-            <HiddenCond condition={isMobile ? !!offerName : false} implementation='css'>
-                <OfferList/>
-            </HiddenCond>
-            <HiddenCond condition={isMobile ? !offerName : !offerName} implementation='css'>
-                <OfferDetails/>
-            </HiddenCond>
-        </div>
-    )
-}
+  const response = await axios({
+    method: "GET",
+    url: `https://api.bnmagnum.pl${query}`,
+  });
 
-OfferSearch.displayName = 'Wyszukiwarka ofert'
-OfferSearch.routeName = '/wyszukiwarka-ofert/[[...offerName]]'
+  return {
+    props: response.data,
+  };
+};
 
-export default OfferSearch
+OfferSearch.displayName = "Wyszukiwarka ofert";
+OfferSearch.routeName = "/wyszukiwarka-ofert/[[...offerName]]";
+
+export default OfferSearch;
