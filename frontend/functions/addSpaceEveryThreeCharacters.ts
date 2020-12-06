739ed0fe03909a5e572fc29f1a21f8cd1b5819b4
Piotr Bartoszewski <bartoszewskip1000@gmail.com>
2020-12-06 01:45:34 +0100

    Fix the phone number formatting issue

--- frontend/functions/addSpaceEveryThreeCharacters.ts
+++ frontend/functions/addSpaceEveryThreeCharacters.ts
@@ -1,5 +1,5 @@
 export const addSpaceEveryThreeCharacters = (str: string) =>
-  [...str]
+  [...((str as any).replaceAll(" ", "") as string)]
     .reverse()
     .map((d, i) => (i % 3 == 0 ? d + " " : d))
     .reverse()
