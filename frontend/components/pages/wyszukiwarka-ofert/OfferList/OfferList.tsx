7f4245c6ebaac11f2c4a4bc6a726d6a144897b0f
Piotr Bartoszewski <bartoszewskip1000@gmail.com>
2020-12-08 11:11:50 +0100

    Add pagination (only ui)

--- frontend/components/pages/wyszukiwarka-ofert/OfferList/OfferList.tsx
+++ frontend/components/pages/wyszukiwarka-ofert/OfferList/OfferList.tsx
@@ -1,4 +1,7 @@
-import { FC } from "react";
+import { css } from "@emotion/core";
+import { useTheme } from "@material-ui/core/styles";
+import { Pagination } from "@material-ui/lab";
+import React, { FC } from "react";
 import { Offer, OfferProps } from "./Offer/Offer";
 
 interface OfferListProps {
@@ -6,11 +9,24 @@ interface OfferListProps {
 }
 
 export const OfferList: FC<OfferListProps> = (props) => {
+  const theme = useTheme();
+  const styles = {
+    root: css`
+      ${theme.customMixins.flexCentered};
+      flex-direction: column;
+    `,
+    pagination: css`
+      display: inline-block;
+      margin: ${theme.spacing(2)}px auto;
+    `,
+  };
+
   return (
-    <div>
+    <div css={styles.root}>
       {props.offers.map((offer) => (
         <Offer key={offer.normal.id} {...offer} />
       ))}
+      <Pagination css={styles.pagination} count={10} color="secondary" />
     </div>
   );
 };
