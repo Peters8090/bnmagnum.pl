7c8a40d0d1a928c54276aae18e7aae17584fbd35
Piotr Bartoszewski <bartoszewskip1000@gmail.com>
2020-12-15 12:24:39 +0100

    Add route animations

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,27 +1,43 @@
-import {AppProps} from 'next/app'
-import {Layout} from '../hoc/Layout/Layout'
-import {Theme} from '../hoc/Theme/Theme'
-import {useEffect} from 'react'
+import { motion } from "framer-motion";
+import { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { Layout } from "../hoc/Layout/Layout";
+import { Theme } from "../hoc/Theme/Theme";
 
-const App = ({Component, pageProps}: AppProps) => {
-    // const router = useRouter()
-    useEffect(() => {
-        // material-ui SSR setup
-        const jssStyles = document.querySelector('#jss-server-side')
-        if (jssStyles) {
-            jssStyles.parentElement!.removeChild(jssStyles)
-        }
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+  useEffect(() => {
+    // material-ui SSR setup
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement!.removeChild(jssStyles);
+    }
 
-        // router.events.on('routeChangeStart', () => window.scrollTo(0, 0))
-    }, [])
+    // router.events.on('routeChangeStart', () => window.scrollTo(0, 0))
+  }, []);
 
-    return (
-        <Theme>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </Theme>
-    )
-}
+  return (
+    <Theme>
+      <Layout>
+        <motion.div
+          key={router.route}
+          initial="pageInitial"
+          animate="pageAnimate"
+          variants={{
+            pageInitial: {
+              opacity: 0,
+            },
+            pageAnimate: {
+              opacity: 1,
+            },
+          }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </Layout>
+    </Theme>
+  );
+};
 
-export default App
+export default App;
