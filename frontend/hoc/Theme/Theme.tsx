043f6308b2217134ceb7014dafe15a22a33c94b2
Piotr Bartoszewski <bartoszewskip1000@gmail.com>
2020-12-18 10:53:23 +0100

    Change language in material ui theme

--- frontend/hoc/Theme/Theme.tsx
+++ frontend/hoc/Theme/Theme.tsx
@@ -1,47 +1,50 @@
-import {FC} from 'react'
-import {StylesProvider, ThemeProvider} from '@material-ui/core/styles'
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
-import responsiveFontSizes from '@material-ui/core/styles/responsiveFontSizes'
-import {GlobalStyles} from './GlobalStyles'
-import {SerializedStyles} from '@emotion/core'
-import css from '@emotion/css'
+import { SerializedStyles } from "@emotion/core";
+import css from "@emotion/css";
+import { plPL } from "@material-ui/core/locale";
+import { StylesProvider, ThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import responsiveFontSizes from "@material-ui/core/styles/responsiveFontSizes";
+import { FC } from "react";
+import { GlobalStyles } from "./GlobalStyles";
 
-declare module '@material-ui/core/styles/createMuiTheme' {
-    interface Theme {
-        customMixins: {
-            flexCentered: SerializedStyles,
-        }
-    }
+declare module "@material-ui/core/styles/createMuiTheme" {
+  interface Theme {
+    customMixins: {
+      flexCentered: SerializedStyles;
+    };
+  }
 
-    // allow configuration using `createMuiTheme`
-    interface ThemeOptions extends Theme {
-    }
+  // allow configuration using `createMuiTheme`
+  interface ThemeOptions extends Theme {}
 }
 
-
-export const Theme: FC = props => {
-    const theme = responsiveFontSizes(createMuiTheme({
+export const Theme: FC = (props) => {
+  const theme = responsiveFontSizes(
+    createMuiTheme(
+      {
         palette: {
-            primary: {main: '#f50057'},
-            secondary: {main: '#1976d2'},
+          primary: { main: "#f50057" },
+          secondary: { main: "#1976d2" },
         },
         customMixins: {
-            flexCentered: css`
-              display: flex;
-              justify-content: center;
-              align-items: center;
-            `,
+          flexCentered: css`
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          `,
         },
-    }), {
-        factor: 2,
-    })
+      },
+      plPL
+    ),
+    {
+      factor: 2,
+    }
+  );
 
-    return (
-        <StylesProvider injectFirst>
-            <GlobalStyles/>
-            <ThemeProvider theme={theme}>
-                {props.children}
-            </ThemeProvider>
-        </StylesProvider>
-    )
-}
+  return (
+    <StylesProvider injectFirst>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
+    </StylesProvider>
+  );
+};
