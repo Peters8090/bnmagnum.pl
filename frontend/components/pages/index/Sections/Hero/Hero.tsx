286f84bbd427d6e0b8685a32de1499b393654c67
Piotr Bartoszewski <bartoszewskip1000@gmail.com>
2020-12-18 16:38:47 +0100

    Hash auto change works perfectly

--- frontend/components/pages/index/Sections/Hero/Hero.tsx
+++ frontend/components/pages/index/Sections/Hero/Hero.tsx
@@ -1,37 +1,37 @@
-import {css} from '@emotion/core'
-import {HeroWelcome} from './HeroWelcome/HeroWelcome'
-import {HeroImage} from './HeroImage/HeroImage'
-import {GoDownFab} from './GoDownFab/GoDownFab'
-import {Hidden} from '@material-ui/core'
-import {useTheme} from '@material-ui/core/styles'
-import {RouteType} from '../../../../../interfaces and types/RouteType'
+import { css } from "@emotion/core";
+import { Hidden } from "@material-ui/core";
+import { useTheme } from "@material-ui/core/styles";
+import { RouteType } from "../../../../../interfaces and types/RouteType";
+import { GoDownFab } from "./GoDownFab/GoDownFab";
+import { HeroImage } from "./HeroImage/HeroImage";
+import { HeroWelcome } from "./HeroWelcome/HeroWelcome";
 
 export const Hero: RouteType = () => {
-    const theme = useTheme()
-    const styles = {
-        root: css`
-			position: relative;
-			display: grid;
-			grid-template-columns: 1fr 1fr;
-			padding: 0 ${theme.spacing(2)}px;
-			
-			${theme.breakpoints.down('sm')} {
-			    ${theme.customMixins.flexCentered};
-			    flex-direction: column;
-			}
-		`,
-    }
+  const theme = useTheme();
+  const styles = {
+    root: css`
+      position: relative;
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      padding: 0 ${theme.spacing(2)}px;
 
-    return (
-        <div css={styles.root}>
-            <HeroWelcome/>
-            <Hidden smDown>
-                <HeroImage/>
-            </Hidden>
-            <GoDownFab/>
-        </div>
-    )
-}
+      ${theme.breakpoints.down("sm")} {
+        ${theme.customMixins.flexCentered};
+        flex-direction: column;
+      }
+    `,
+  };
 
-Hero.displayName = 'Home'
-Hero.routeName = '/'
+  return (
+    <div css={styles.root}>
+      <HeroWelcome />
+      <Hidden smDown>
+        <HeroImage />
+      </Hidden>
+      <GoDownFab />
+    </div>
+  );
+};
+
+Hero.displayName = "Home";
+Hero.routeName = "/";
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,36 +1,65 @@
-import {css} from '@emotion/core'
-import {Hero} from '../components/pages/index/Sections/Hero/Hero'
-import {About} from '../components/pages/index/Sections/About/About'
-import {OurEmployees} from '../components/pages/index/Sections/OurEmployees/OurEmployees'
-import {Footer} from '../components/pages/index/Sections/Footer/Footer'
-import {RouteType} from '../interfaces and types/RouteType'
-import {useCurrentNavigationHeight} from '../hooks/useCurrentNavigationHeight'
+import { css } from "@emotion/core";
+import { useEffect } from "react";
+import { About } from "../components/pages/index/Sections/About/About";
+import { Footer } from "../components/pages/index/Sections/Footer/Footer";
+import { Hero } from "../components/pages/index/Sections/Hero/Hero";
+import { OurEmployees } from "../components/pages/index/Sections/OurEmployees/OurEmployees";
+import { useCurrentNavigationHeight } from "../hooks/useCurrentNavigationHeight";
+import { RouteType } from "../interfaces and types/RouteType";
 
 const HomePage: RouteType = () => {
-    const navHeight = useCurrentNavigationHeight()
-    const styles = {
-        root: css`
-		    display: flex;
-		    flex-direction: column;
-		    
-		    & > * {
-		      min-height: calc(100vh - ${navHeight}px);
-		    }
-		`,
-    }
+  const navHeight = useCurrentNavigationHeight();
+  const styles = {
+    root: css`
+      display: flex;
+      flex-direction: column;
 
-    return (
-        <div css={styles.root}>
-            <Hero/>
-            <About/>
-            <OurEmployees/>
-            <Footer/>
-        </div>
-    )
-}
+      & > * {
+        min-height: calc(100vh - ${navHeight}px);
+      }
+    `,
+  };
 
-HomePage.displayName = 'Home'
-HomePage.routeName = '/'
+  const sections = ["", "o-firmie", "nasi-pracownicy", "kontakt"];
 
+  const findClosestValue = (counts: number[], goal: number) =>
+    counts.reduce((prev, curr) =>
+      Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev
+    );
 
-export default HomePage
+  useEffect(() => {
+    setInterval(() => {
+      const scrollY = document?.scrollingElement?.scrollTop;
+
+      if (scrollY !== undefined) {
+        const scrollTops = sections.map((section) =>
+          section === "" ? 0 : document.getElementById(section)!.offsetTop
+        );
+
+        const closestSection =
+          sections[scrollTops.indexOf(findClosestValue(scrollTops, scrollY))];
+
+        history.replaceState(
+          null,
+          "",
+          document.location.pathname +
+            (closestSection ? `#${closestSection}` : "")
+        );
+      }
+    }, 500);
+  }, []);
+
+  return (
+    <div css={styles.root}>
+      <Hero />
+      <About />
+      <OurEmployees />
+      <Footer />
+    </div>
+  );
+};
+
+HomePage.displayName = "Home";
+HomePage.routeName = "/";
+
+export default HomePage;
