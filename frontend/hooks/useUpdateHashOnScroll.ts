720e8a9e7e194c5536cd694c8ca29f7f24bdc65f
Piotr Bartoszewski <bartoszewskip1000@gmail.com>
2020-12-21 16:15:18 +0100

    Refactor use update hash on scroll

--- frontend/hooks/useUpdateHashOnScroll.ts
+++ frontend/hooks/useUpdateHashOnScroll.ts
@@ -4,10 +4,7 @@ import { useEffect } from "react";
  * @param sections - Section IDs you want to update on scroll
  */
 export const useUpdateHashOnScroll = (sections: string[]) => {
-  const _sections = [
-    "",
-    ...sections.filter((s) => !!document.getElementById(s)),
-  ];
+  const _sections = sections.filter((s) => !!document.getElementById(s));
 
   const findClosestValue = (counts: number[], goal: number) =>
     counts.reduce((prev, curr) =>
@@ -19,20 +16,28 @@ export const useUpdateHashOnScroll = (sections: string[]) => {
       const scrollY = document?.scrollingElement?.scrollTop;
 
       if (scrollY !== undefined) {
-        const scrollTops = _sections.map((section) =>
-          section === "" ? 0 : document.getElementById(section)!.offsetTop
-        );
-
-        const closestSection =
-          _sections[scrollTops.indexOf(findClosestValue(scrollTops, scrollY))];
-
-        if (window.location.hash.slice(1) !== closestSection) {
+        const updateHash = (hash: string) => {
           history.replaceState(
             null,
             "",
-            document.location.pathname +
-              (closestSection ? `#${closestSection}` : "")
+            document.location.pathname + (hash ? `#${hash}` : "")
           );
+        };
+        if (scrollY === 0) {
+          updateHash("");
+        } else {
+          const scrollTops = _sections.map(
+            (section) => document.getElementById(section)!.offsetTop
+          );
+
+          const closestSection =
+            _sections[
+              scrollTops.indexOf(findClosestValue(scrollTops, scrollY))
+            ];
+
+          if (window.location.hash.slice(1) !== closestSection) {
+            updateHash(closestSection);
+          }
         }
       }
     }, 400);
