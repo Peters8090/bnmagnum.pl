042fb27c3d12a91dab0ca03dff9a41bd21533c16
Piotr Bartoszewski <bartoszewskip1000@gmail.com>
2020-12-23 14:27:16 +0100

    Get rid of the unused django backend

--- backend/listings/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from djmoney.models.fields import MoneyField
-
-
-class Listing(models.Model):
-    price = MoneyField(max_digits=10, decimal_places=0, default_currency="PLN")
-    total_surface = models.DecimalField(max_digits=10, decimal_places=2)
-    land_area = models.DecimalField(max_digits=10, decimal_places=2)
