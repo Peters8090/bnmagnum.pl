b1b4562661db4f1a527f47da52edbc10d2a08bfd
Piotr Bartoszewski <bartoszewskip1000@gmail.com>
2020-12-26 21:23:31 +0100

    Remove redundant code in AboutSteps

--- components/pages/index/Sections/AboutSteps/AboutSteps.tsx
+++ components/pages/index/Sections/AboutSteps/AboutSteps.tsx
@@ -33,6 +33,7 @@ export const AboutSteps: RouteType = () => {
       margin-bottom: theme.spacing(1) px;
     `,
   };
+
   const steps = [
     "Select campaign settings",
     "Create an ad group",
@@ -40,48 +41,15 @@ export const AboutSteps: RouteType = () => {
   ];
 
   const [activeStep, setActiveStep] = useState(0);
-  const [skipped, setSkipped] = useState(new Set<number>());
 
-  const isStepOptional = (step: number) => {
-    return step === 1;
-  };
-
-  const isStepSkipped = (step: number) => {
-    return skipped.has(step);
-  };
-
-  const handleNext = () => {
-    let newSkipped = skipped;
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values());
-      newSkipped.delete(activeStep);
-    }
-
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped(newSkipped);
+  const handleNextOrReset = () => {
+    setActiveStep((prevActiveStep) =>
+      prevActiveStep === steps.length - 1 ? 0 : prevActiveStep + 1
+    );
   };
 
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleSkip = () => {
-    if (!isStepOptional(activeStep)) {
-      // You probably want to guard against something like this,
-      // it should never occur unless someone's actively trying to break something.
-      throw new Error("You can't skip a step that isn't optional.");
-    }
-
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped((prevSkipped) => {
-      const newSkipped = new Set(prevSkipped.values());
-      newSkipped.add(activeStep);
-      return newSkipped;
-    });
-  };
-
-  const handleReset = () => {
-    setActiveStep(0);
   };
 
   const linkId = convertRouteHashToLinkId(AboutSteps.routeName);
@@ -89,62 +57,27 @@ export const AboutSteps: RouteType = () => {
   return (
     <div css={styles.root} id={linkId}>
       <Stepper activeStep={activeStep}>
-        {steps.map((label, index) => {
-          const stepProps: { completed?: boolean } = {};
-          const labelProps: {
-            optional?: React.ReactNode;
-          } = {};
-          if (isStepOptional(index)) {
-            labelProps.optional = (
-              <Typography variant="caption">Optional</Typography>
-            );
-          }
-          if (isStepSkipped(index)) {
-            stepProps.completed = false;
-          }
-          return (
-            <Step key={label} {...stepProps}>
-              <StepLabel {...labelProps}>{label}</StepLabel>
-            </Step>
-          );
-        })}
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
       </Stepper>
-      {activeStep === steps.length ? (
-        <React.Fragment>
-          <Typography css={styles.instructions}>
-            All steps completed - you&apos;re finished
-          </Typography>
-          <div css={styles.buttonWrapper}>
-            <div css={styles.spacer} />
-            <Button onClick={handleReset}>Reset</Button>
-          </div>
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          <Typography css={styles.instructions}>
-            Step {activeStep + 1}
-          </Typography>
-          <div css={styles.buttonWrapper}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              css={styles.button}
-            >
-              Back
-            </Button>
-            <div css={styles.spacer} />
-            {isStepOptional(activeStep) && (
-              <Button color="inherit" onClick={handleSkip} css={styles.button}>
-                Skip
-              </Button>
-            )}
-            <Button onClick={handleNext}>
-              {activeStep === steps.length - 1 ? "Finish" : "Next"}
-            </Button>
-          </div>
-        </React.Fragment>
-      )}
+      <Typography css={styles.instructions}>Step {activeStep + 1}</Typography>
+      <div css={styles.buttonWrapper}>
+        <Button
+          color="inherit"
+          disabled={activeStep === 0}
+          onClick={handleBack}
+          css={styles.button}
+        >
+          Back
+        </Button>
+        <div css={styles.spacer} />
+        <Button onClick={handleNextOrReset}>
+          {activeStep === steps.length - 1 ? "Reset" : "Next"}
+        </Button>
+      </div>
     </div>
   );
 };
