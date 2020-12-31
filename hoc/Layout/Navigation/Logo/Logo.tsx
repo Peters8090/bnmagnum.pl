e5cacf22017187b9000c10a29cd288039cb28fa9
Piotr Bartoszewski <bartoszewskip1000@gmail.com>
2020-12-31 18:06:26 +0100

    Increase logo size

--- hoc/Layout/Navigation/Logo/Logo.tsx
+++ hoc/Layout/Navigation/Logo/Logo.tsx
@@ -1,12 +1,20 @@
 import { css } from "@emotion/core";
+import { useTheme } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import { FC } from "react";
 import { Content } from "../../../../content";
 
 export const Logo: FC = () => {
+  const theme = useTheme();
   const styles = {
     imgRoot: css`
-      height: 40px;
+      height: 65px;
+
+      ${theme.breakpoints.down("sm")} {
+        height: 50px;
+      }
+
+      margin: ${theme.spacing(2)}px 0;
     `,
     textRoot: css`
       font-family: "Cabin", sans-serif;
