49f4c8341c32ac57472a101243d620dfb7268be4
Piotr Bartoszewski <bartoszewskip1000@gmail.com>
2021-01-03 21:25:48 +0100

    Refactor NextLinkSmoothScroll

--- hoc/Layout/Navigation/NavigationItems/NavigationItem/NextLinkSmoothScroll/NextLinkSmoothScroll.tsx
+++ hoc/Layout/Navigation/NavigationItems/NavigationItem/NextLinkSmoothScroll/NextLinkSmoothScroll.tsx
@@ -5,22 +5,27 @@ import { LayoutContext } from "../../../../Layout";
 // source: https://gist.github.com/vinaypuppal/b7271ad84a0d69c9cfafaaa83afed199
 
 // Get the top position of an element in the document
-const getTop = function (element: any, start: any) {
-  // return value of html.getBoundingClientRect().top ... IE : 0, other browsers : -pageYOffset
-  if (element.nodeName === "HTML") return -start;
-  return element.getBoundingClientRect().top + start;
-};
+const getTop = (element: HTMLElement, start: number) =>
+  element.nodeName === "HTML"
+    ? -start
+    : element.getBoundingClientRect().top + start;
+// return value of html.getBoundingClientRect().top ... IE : 0, other browsers : -pageYOffset
+
 // ease in out function thanks to:
 // http://blog.greweb.fr/2012/02/bezier-curve-based-easing-functions-from-concept-to-implementation/
-const easeInOutCubic = function (t: any) {
-  return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
-};
+const easeInOutCubic = (t: number) =>
+  t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
 
 // calculate the scroll position we should be in
 // given the start and end point of the scroll
 // the time elapsed from the beginning of the scroll
 // and the total duration of the scroll (default 500ms)
-const position = function (start: any, end: any, elapsed: any, duration: any) {
+const position = function (
+  start: number,
+  end: number,
+  elapsed: number,
+  duration: number
+) {
   if (elapsed > duration) return end;
   return start + (end - start) * easeInOutCubic(elapsed / duration); // <-- you can change the easing funtion there
   // return start + (end - start) * (elapsed / duration); // <-- this would give a linear scroll
@@ -31,16 +36,13 @@ const position = function (start: any, end: any, elapsed: any, duration: any) {
 // if the first argument is numeric then scroll to this location
 // if the callback exist, it is called when the scrolling is finished
 // if context is set then scroll that element, else scroll window
-const smoothScroll2 = function (
-  el: any,
-  duration: any,
-  callback: any,
-  context: any
-) {
-  duration = duration || 250;
-  context = context || window;
-  const start = context.scrollTop || window.pageYOffset;
-  let end: any;
+const smoothScroll2 = (
+  el: HTMLElement | number,
+  duration: number = 250,
+  callback: Function
+) => {
+  const start = window.pageYOffset;
+  let end: number;
   if (typeof el === "number") {
     end = el - 60;
   } else {
@@ -51,43 +53,37 @@ const smoothScroll2 = function (
   const requestAnimationFrame =
     window.requestAnimationFrame ||
     window.webkitRequestAnimationFrame ||
-    function (fn) {
+    ((fn) => {
       window.setTimeout(fn, 15);
-    };
+    });
 
-  const step = function () {
+  const step = () => {
     const elapsed = Date.now() - clock;
-    if (context !== window) {
-      context.scrollTop = position(start, end, elapsed, duration);
-    } else {
-      window.scroll(0, position(start, end, elapsed, duration));
-    }
+    window.scroll(0, position(start, end, elapsed, duration));
 
     if (elapsed > duration) {
-      if (typeof callback === "function") {
-        callback(el);
-      }
+      callback(el);
     } else {
       requestAnimationFrame(step);
     }
   };
   step();
 };
-const smoothScroll = (url: any) => {
-  return new Promise(function (resolve) {
+
+const smoothScroll = (url: string) =>
+  new Promise((resolve) => {
     const id = url.substr(2);
     const el = document.getElementById(id);
     if (el) {
-      smoothScroll2(el, 600, resolve, undefined);
+      smoothScroll2(el, 600, resolve);
     } else {
-      smoothScroll2(0, 600, resolve, undefined);
+      smoothScroll2(0, 600, resolve);
     }
   });
-};
 
 interface NextLinkSmoothScrollProps {
-  href: any;
-  as: any;
+  href: string;
+  as: string;
   children: JSX.Element;
 }
 
@@ -95,20 +91,15 @@ export const NextLinkSmoothScroll: FC<NextLinkSmoothScrollProps> = (props) => {
   const layoutContext = useContext(LayoutContext);
 
   const linkClicked = async () => {
-    const scrollX = window.pageXOffset;
-    const scrollY = window.pageYOffset;
-
-    const location = window.location;
-    const href = props.href;
-
     layoutContext.setIsNavigatingToHash(true);
 
-    if (location.pathname === href.split("#")[0]) {
-      window.scrollTo(scrollX, scrollY);
+    // figure out if the page has to be changed
+    if (window.location.pathname === props.href.split("#")[0]) {
       await smoothScroll(props.href);
     } else {
       await Router.push(props.href, props.as);
     }
+
     layoutContext.setIsNavigatingToHash(false);
   };
 
