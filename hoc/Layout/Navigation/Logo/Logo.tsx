a4e0a4ae4517366c34a5616915c26c9e96b85253
Piotr Bartoszewski <bartoszewskip1000@gmail.com>
2021-01-05 17:37:54 +0100

    Attempt to fix images in production

--- hoc/Layout/Navigation/Logo/Logo.tsx
+++ hoc/Layout/Navigation/Logo/Logo.tsx
@@ -1,6 +1,7 @@
 import { css } from "@emotion/core";
 import { useTheme } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import Image from "next/image";
 import { FC } from "react";
 import { Content } from "../../../../content";
 
@@ -23,7 +24,13 @@ export const Logo: FC = () => {
 
   if (Content.navigation.logoImage) {
     return (
-      <img src={Content.navigation.logoImage} alt="logo" css={styles.imgRoot} />
+      <Image
+        width={60}
+        height={60}
+        src={Content.navigation.logoImage}
+        alt="logo"
+        css={styles.imgRoot}
+      />
     );
   } else {
     return (
