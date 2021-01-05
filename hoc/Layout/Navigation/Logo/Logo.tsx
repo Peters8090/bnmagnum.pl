c0b6f0e2af1338f3f88a2803a78cb6b8c0af8dc3
Piotr Bartoszewski <bartoszewskip1000@gmail.com>
2021-01-05 17:37:54 +0100

    Revert "Attempt to fix images in production"
    This reverts commit a4e0a4ae4517366c34a5616915c26c9e96b85253.

--- hoc/Layout/Navigation/Logo/Logo.tsx
+++ hoc/Layout/Navigation/Logo/Logo.tsx
@@ -1,7 +1,6 @@
 import { css } from "@emotion/core";
 import { useTheme } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import Image from "next/image";
 import { FC } from "react";
 import { Content } from "../../../../content";
 
@@ -24,13 +23,7 @@ export const Logo: FC = () => {
 
   if (Content.navigation.logoImage) {
     return (
-      <Image
-        width={60}
-        height={60}
-        src={Content.navigation.logoImage}
-        alt="logo"
-        css={styles.imgRoot}
-      />
+      <img src={Content.navigation.logoImage} alt="logo" css={styles.imgRoot} />
     );
   } else {
     return (
