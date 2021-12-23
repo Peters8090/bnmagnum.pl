68d6cace4777f021f7b87fb86ce4b36fb52a13a2
Piotr Bartoszewski <bartoszewskip1000@gmail.com>
2021-12-23 23:35:20 +0100

    Move id up in the details section of an offer

--- components/pages/wyszukiwarka-ofert/OfferDetails/Section/Sections/DetailsSection/DetailsSection.tsx
+++ components/pages/wyszukiwarka-ofert/OfferDetails/Section/Sections/DetailsSection/DetailsSection.tsx
@@ -25,23 +25,33 @@ export const DetailsSection: FC<DetailsSectionProps> = (props) => {
     `,
   };
 
+  const paramsSorted = (
+    Object.entries(props.params) as [string, string | number][]
+  ).sort();
+
+  // move id param up
+
+  const nrIndex = paramsSorted.findIndex(([k]) => k === "Numer oferty");
+
+  if (nrIndex !== -1) {
+    paramsSorted.unshift(paramsSorted.splice(nrIndex, 1)[0]);
+  }
+
   return (
     <Section title="Szczegóły">
-      {(Object.entries(props.params) as [string, string | number][])
-        .sort()
-        .map((el) => (
-          <div>
-            <div css={styles.detailText}>
-              <Typography align="left" css={styles.detailTextName} variant="h6">
-                {el[0]}
-              </Typography>
-              <Typography align="right" color="textSecondary" variant="h6">
-                {el[1]}
-              </Typography>
-            </div>
-            <Divider css={styles.detailDivider} />
+      {paramsSorted.map((el) => (
+        <div>
+          <div css={styles.detailText}>
+            <Typography align="left" css={styles.detailTextName} variant="h6">
+              {el[0]}
+            </Typography>
+            <Typography align="right" color="textSecondary" variant="h6">
+              {el[1]}
+            </Typography>
           </div>
-        ))}
+          <Divider css={styles.detailDivider} />
+        </div>
+      ))}
     </Section>
   );
 };
